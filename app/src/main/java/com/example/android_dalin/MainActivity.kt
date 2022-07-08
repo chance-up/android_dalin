67f5cc63b4d31da857d445412388ac42ad516158
chance-up <ccs6473@naver.com>
2022-07-08 17:09:35 +0900

    Update MainActivity.kt
    todo ...

--- app/src/main/java/com/example/android_dalin/MainActivity.kt
+++ app/src/main/java/com/example/android_dalin/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+// todo 1. tab controller
+// todo 2. recyclerview 
+// todo 3. viewmodel 적용
     }
 }
