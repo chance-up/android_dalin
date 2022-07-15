b28f2710f69dcbaa1365683165b16744cce39f88
chance-up <ccs6473@naver.com>
2022-07-15 16:33:46 +0900

    feat : bottom nav 추가 완료.. 스플래시 화면 구현예정

--- app/src/main/java/com/example/android_dalin/ui/BaseFragment.kt
+++ app/src/main/java/com/example/android_dalin/ui/BaseFragment.kt
@@ -1,24 +1,71 @@
 package com.example.android_dalin.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android_dalin.R
+import com.example.android_dalin.databinding.FragmentBaseBinding
 
 
 class BaseFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var baseFragmentBinding: FragmentBaseBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false)
+        baseFragmentBinding = FragmentBaseBinding.inflate(inflater, container, false)
+        return baseFragmentBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.let {
+            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, HomeFragment()).commit()
+        }
+
+        baseFragmentBinding.bottomNavigationView.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> {
+                        Log.d("msg", "home")
+                        activity?.let {
+                            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, HomeFragment()).commit()
+                        }
+                    }
+                    R.id.estimate -> {
+                        Log.d("msg", "est")
+                        activity?.let {
+                            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, EstimateFragment()).commit()
+                        }
+                    }
+                    R.id.search -> {
+                        Log.d("msg", "search")
+                        activity?.let {
+                            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, SearchFragment()).commit()
+                        }
+                    }
+                    R.id.chat -> {
+                        Log.d("msg", "chat")
+                        activity?.let {
+                            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, ChatFragment()).commit()
+                        }
+                    }
+                    R.id.profile -> {
+                        Log.d("msg", "profile")
+                        activity?.let {
+                            it.supportFragmentManager.beginTransaction().replace(baseFragmentBinding.layoutView.id, ProfileFragment()).commit()
+                        }
+                    }
+                }
+                true
+            }
+        }
+    }
 }
