8e126e005905286ec7a2968906a4d5f9f5c6b4dc
chance-up <ccs6473@naver.com>
2022-07-20 13:27:32 +0900

    add : splash sample image

--- app/src/main/java/com/example/android_dalin/ui/HomeFragment.kt
+++ app/src/main/java/com/example/android_dalin/ui/HomeFragment.kt
@@ -29,10 +29,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = homeFragmentBinding.advertiseViewpager
-//        val tabLayout = homeFragmentBinding.advertiseIndicatorTablayout
         viewPager.adapter=ViewPagerAdapter()
-
-
     }
-
 }
